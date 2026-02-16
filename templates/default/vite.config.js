d23f47800bd480bc67f1e87ddafb2df0a838c75c
Ilia Naryzhny <phantom@ydn.ru>
2026-02-15 18:43:54 -0800

    Allow all hosts in Vite dev server config

--- templates/default/vite.config.js
+++ templates/default/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: 'src',
+  server: {
+    allowedHosts: true
+  },
   build: {
     outDir: '../dist',
     emptyOutDir: true
